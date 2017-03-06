334bc7783aa9c95cf352a89a32039e1f8a987d53
franaln <franaln@gmail.com>
2017-03-06 14:28:49 +0100

    update summary rucio

--- summary_rucio.py
+++ summary_rucio.py
@@ -1,3 +1,5 @@
+#! /usr/bin/env python
+
 import sys
 
 s = open(sys.argv[1]).read()
@@ -11,14 +13,17 @@ for index, line in enumerate(lines):
 
     if not line.startswith('----------------------------------------'):
         continue
-        
-    d = {
-        'name': lines[index+1][4:],
-        'total':  int(lines[index+2].split(':')[1].strip()),
-        'downloaded': int(lines[index+3].split(':')[1].strip()),
-        'local': int(lines[index+4].split(':')[1].strip()),
-        'error': int(lines[index+5].split(':')[1].strip()),
-        }
+    
+    try:
+        d = {
+            'name': lines[index+1][4:],
+            'total':  int(lines[index+2].split(':')[1].strip()),
+            'downloaded': int(lines[index+3].split(':')[1].strip()),
+            'local': int(lines[index+4].split(':')[1].strip()),
+            'error': int(lines[index+5].split(':')[1].strip()),
+            }
+    except:
+        raise        
 
     if len(d['name']) > max_sample_lenght:
         max_sample_lenght = len(d['name'])
@@ -27,11 +32,15 @@ for index, line in enumerate(lines):
 
 
 errors = []
+empty  = []
 
 print 'rucio summary:'
 for d in sample_info:
-    if d['error'] > 0:
+    if d['error'] > 0 or d['local'] + d['downloaded'] < d['total']:
         errors.append(d)
+
+    if d['total'] == 0:
+        empty.append(d)
 
     ss = '{name: <%s}: total={total:<3}, downloaded={downloaded: <3}, local={local: <3}, error={error: <3}' % max_sample_lenght
 
@@ -43,4 +52,12 @@ if errors:
         print e['name']
 else:
     print '# no errors'
+
+if empty:
+    print '# the following samples are empty:'
+    for e in empty:
+        print e['name']
+else:
+    print '# no empty samples'
+
 
